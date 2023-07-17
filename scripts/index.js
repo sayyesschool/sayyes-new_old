8d64f746317f65e7e79498d6fcb3dae6368f8602
firefoxic <firefoxic.dev@gmail.com>
2023-07-17 17:38:55 +0000

    Deploying to gh-pages from @ sayyesschool/sayyes-new@8646d5078f3a146178ce7b61bc434167df6f993e 🚀

---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+import{getHeaderBlockSize,toggleMainHeaderView}from"./header.js";import{Tabs}from"./tabs.js";getHeaderBlockSize(),window.addEventListener("scroll",toggleMainHeaderView()),window.addEventListener("load",(()=>{document.querySelectorAll("[role=tablist]").forEach((e=>{new Tabs(e)}))}));
